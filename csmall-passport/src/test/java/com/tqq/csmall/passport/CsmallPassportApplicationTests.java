dc5a1574dbc177fe0974c252f9eed1be1d3741b9
tqq <1157595131@qq.com>
2023-05-18 15:54:42 +0800

    0.0.2

--- csmall-passport/src/test/java/com/tqq/csmall/passport/CsmallPassportApplicationTests.java
+++ csmall-passport/src/test/java/com/tqq/csmall/passport/CsmallPassportApplicationTests.java
@@ -1,13 +1,21 @@
 package com.tqq.csmall.passport;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class CsmallPassportApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        dataSource.getConnection();
     }
 
 }
